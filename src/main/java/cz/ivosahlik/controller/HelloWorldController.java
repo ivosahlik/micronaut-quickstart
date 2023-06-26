84319a631cfd1ad9ea3a3f145ef5aff1dd98f8ef
ivosahlik <ivosahlik@seznam.cz>
2023-06-27 00:47:12 +0200

    added logging by logback

--- src/main/java/cz/ivosahlik/controller/HelloWorldController.java
+++ src/main/java/cz/ivosahlik/controller/HelloWorldController.java
@@ -5,9 +5,13 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import jakarta.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller("/helloworld")
 public class HelloWorldController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWorldController.class);
 
     @Inject
     private MyService myService;
@@ -20,6 +24,7 @@ public class HelloWorldController {
 
     @Get(produces = MediaType.TEXT_PLAIN)
     public String helloWorld() {
+        LOG.debug("Called the hello World API.");
         return myService.helloFromService();
     }
 
